2e45a660f97e3e4e4f5cedf28d8cbf783cdcf874
Riccardo Iaconelli <riccardo@kde.org>
2021-12-17 17:07:42 +0100

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -3,6 +3,6 @@ from setuptools import setup
 setup(name='wtl',
       version='0.1',
       description='WikiToLearn Utils',
-      url='https://github.com/WikiToLearn/python35',
+      url='https://github.com/WikiToLearn/python38',
       packages=['wtl'],
       zip_safe=False)
